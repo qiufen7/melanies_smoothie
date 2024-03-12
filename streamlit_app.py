92782dbfa74a4138725d847b981de605b93fbcb7
qiufen7 <163057716+qiufen7@users.noreply.github.com>
2024-03-12 09:33:50 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,5 +45,5 @@ if ingredients_list:
     time_to_insert = st.button("Submit Order")
     
     if time_to_insert:
-        session.sql(my_insert_stmt).lect()
+        session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
